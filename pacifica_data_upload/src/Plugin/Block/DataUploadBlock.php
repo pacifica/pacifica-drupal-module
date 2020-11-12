9cd8e3925c7f28b9e372611db241c76787b52e8f
David Brown <dmlb2000@gmail.com>
2020-11-12 21:57:18 +0000

    Add Custom Data Upload Block
    This adds a custom data upload block with a theme key
    block--data-upload.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

---
+++ pacifica_data_upload/src/Plugin/Block/DataUploadBlock.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\pacifica_data_upload\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a Data Upload Block.
+ *
+ * @Block(
+ *   id = "data_upload",
+ *   admin_label = @Translation("Data Upload Block"),
+ *   category = @Translation("Pacifica Data Upload"),
+ * )
+ */
+class DataUploadBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+    $config = $this->getConfiguration();
+    $form['data_upload_url'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Data Upload API Url'),
+      '#default_value' => (isset($config['data_upload_url']) ? $config['data_upload_url'] : ''),
+      '#size' => 60,
+      '#maxlength' => 128,
+      '#required' => TRUE,
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    // Save our custom settings when the form is submitted.
+    $this->setConfigurationValue('data_upload_url', $form_state->getValue('data_upload_url'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $config = $this->getConfiguration();
+    return [
+      '#data_upload_url' => $config['data_upload_url'],
+    ];
+  }
+
+}
